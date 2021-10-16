b2195335a5909b88a2dd83258752410318809d29
simone-bianco <simone9292@live.com>
2021-10-16 16:17:47 +0200

    + miglioramento test

--- tests/Feature/Whitebox/ReservationRepositoryCoverageTest.php
+++ tests/Feature/Whitebox/ReservationRepositoryCoverageTest.php
@@ -6,6 +6,7 @@ use App\Exceptions\MaxCapacityExceededException;
 use App\Models\Patient;
 use App\Models\Reservation;
 use App\Models\Structure;
+use App\Models\Vaccine;
 use Carbon\Carbon;
 use Database\Factories\ReservationFactory;
 use Exception;
@@ -30,8 +31,11 @@ class ReservationRepositoryCoverageTest extends ReservationTestCase
             $this->fail('Eccezione di validazione non lanciata');
         } catch (ValidationException $validationException) {
             $this->assertNotNull($validationException);
+            $reservation = Reservation::findOrFail($reservation->id);
+            $this->assertNotEquals(Reservation::COMPLETED_STATE, $reservation->state);
         }
     }
+
     /**
      * @throws MaxCapacityExceededException
      * @throws Throwable
@@ -51,6 +55,144 @@ class ReservationRepositoryCoverageTest extends ReservationTestCase
             $this->fail('Lanciata eccezione errata');
         } catch (Exception $exception) {
             $this->assertNotNull($exception);
+            $reservation = Reservation::findOrFail($reservation->id);
+            $this->assertNotEquals(Reservation::COMPLETED_STATE, $reservation->state);
+        }
+    }
+
+    /**
+     * @throws MaxCapacityExceededException
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function testCancelAndStockIncrementThrowsValidationException()
+    {
+        $reservation = $this->createReservation(Structure::firstOrFail(), Patient::firstOrFail());
+        $reservation->state = "NOT_VALID_STATE";
+        try {
+            $this->reservationRepository->cancelAndStockIncrement($reservation);
+            $this->fail('Eccezione di validazione non lanciata');
+        } catch (ValidationException $validationException) {
+            $this->assertNotNull($validationException);
+            $reservation = Reservation::findOrFail($reservation->id);
+            $this->assertNotEquals(Reservation::CANCELED_STATE, $reservation->state);
+        }
+    }
+
+    /**
+     * @throws MaxCapacityExceededException
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function testCreateAndStockDecrementThrowsValidationException()
+    {
+        $reservation = ReservationFactory::new()->make([
+            'patient_id' => 100,
+            'stock_id' => 1,
+            'date' => Carbon::now()->format('Y-m-d')
+        ]);
+        try {
+            $this->reservationRepository->createAndStockDecrement($reservation);
+            $this->fail('Eccezione di validazione non lanciata');
+        } catch (ValidationException $validationException) {
+            $this->assertNotNull($validationException);
+            $this->assertDatabaseCount('reservations', 0);
+        }
+    }
+
+    /**
+     * @throws MaxCapacityExceededException
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function testCreateAndStockDecrementThrowsException()
+    {
+        $reservation = ReservationFactory::new()->make([
+            'patient_id' => 100,
+            'stock_id' => '100',
+            'date' => Carbon::now()->format('Y-m-d')
+        ]);
+        try {
+            $this->reservationRepository->createAndStockDecrement($reservation);
+            $this->fail('Eccezione di validazione non lanciata');
+        } catch (ValidationException $validationException) {
+            $this->fail('Lanciata eccezione errata');
+        } catch (Exception $exception) {
+            $this->assertNotNull($exception);
+            $this->assertDatabaseCount('reservations', 0);
+        }
+    }
+
+    /**
+     * @throws MaxCapacityExceededException
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function testCreateRecallAndStockDecrementThrowsValidationException()
+    {
+        $structure = Structure::firstOrFail();
+        foreach ($structure->stocks as $stock) {
+            $stock->quantity = 10;
+            $stock->save();
+        }
+        $patient = Patient::firstOrFail();
+        $reservation = $this->createReservation($structure, $patient);
+        $this->reservationRepository->confirmAndSave($reservation);
+        $this->reservationRepository->completeAndSave($reservation);
+        try {
+            $this->reservationRepository->createRecallAndStockDecrement(
+                $patient, Carbon::now()->subDays(2), '12:00', Vaccine::firstOrFail(), $structure
+            );
+            $this->fail('Eccezione di validazione non lanciata');
+        } catch (ValidationException $validationException) {
+            $this->assertNotNull($validationException);
+            $this->assertDatabaseCount('reservations', 1);
+        }
+    }
+
+    /**
+     * @throws MaxCapacityExceededException
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function testCreateRecallAndStockDecrementThrowsException()
+    {
+        $structure = Structure::firstOrFail();
+        $patient = Patient::firstOrFail();
+        $reservation = $this->createReservation($structure, $patient);
+        $this->reservationRepository->confirmAndSave($reservation);
+        $this->reservationRepository->completeAndSave($reservation);
+        try {
+            $this->reservationRepository->createRecallAndStockDecrement(
+                $patient, Carbon::now()->format('Y-m-d'), '12:00', Vaccine::firstOrFail(), $structure
+            );
+            $this->fail('Eccezione di validazione non lanciata');
+        } catch (ValidationException $validationException) {
+            $this->fail('Lanciata eccezione errata');
+        } catch (Exception $exception) {
+            $this->assertNotNull($exception);
+            $this->assertDatabaseCount('reservations', 1);
+        }
+    }
+
+    /**
+     * @throws MaxCapacityExceededException
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function testChangeVaccineAndConfirmThrowsValidationException()
+    {
+        $structure = Structure::firstOrFail();
+        $patient = Patient::firstOrFail();
+        $reservation = $this->createReservation($structure, $patient);
+        $this->reservationRepository->confirmAndSave($reservation);
+        try {
+            $this->reservationRepository->changeVaccineAndConfirm(
+                $reservation, Vaccine::findOrFail(2)
+            );
+            $this->fail('Eccezione di validazione non lanciata');
+        } catch (ValidationException $validationException) {
+            $this->assertNotNull($validationException);
         }
     }
 
